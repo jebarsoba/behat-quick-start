527aacb6cc8f0d57ce6e599e4f45ab29da1781c7
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2022-05-11 10:38:49 -0300

    snippets

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -10,13 +11,34 @@ use Behat\Gherkin\Node\TableNode;
 class FeatureContext implements Context
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @Given there is a :arg1, which costs £:arg2
      */
-    public function __construct()
+    public function thereIsAWhichCostsPs($arg1, $arg2)
     {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add the :arg1 to the basket
+     */
+    public function iAddTheToTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 product(s) in the basket
+     */
+    public function iShouldHaveProductInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the overall basket price should be £:arg1
+     */
+    public function theOverallBasketPriceShouldBePs($arg1)
+    {
+        throw new PendingException();
     }
 }
